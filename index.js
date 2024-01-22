c4a5355e4d60c01535c25fabad62d474d008d674
Sohail Akhtar Ali <alisohail2448@gmail.com>
2024-01-22 16:54:17 +0530

    Sohail | Fix: added and solved the changes

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ const app = express();
 app.use(cors());
 
 const doctors = [
-    { id: 1, name: "Dr. Jadhav", expertise: "Orthopedic Surgeon", city: "Mumbai" },
-    { id: 2, name: "Dr. Gawande", expertise: "Cardiologist", city: "Mumbai" },
-    { id: 3, name: "Dr. Patel", expertise: "Dermatologist", city: "Pune" },
-    { id: 4, name: "Dr. Sam", expertise: "Orthologist", city: "Pune" },
-    { id: 5, name: "Dr. AB Patel", expertise: "Cardiologist", city: "Pune" },
-    { id: 6, name: "Dr.Saurabh", expertise: "Neurologist", city: "Pune" },
-    { id: 7, name: "Dr. John Doe", expertise: "Nephrologist", city: "Pune" },
+    { id: 1, image: "https://thumbs.dreamstime.com/b/indian-doctor-stethoscope-around-neck-his-office-170292594.jpg", name: "Dr. Jadhav", expertise: "Orthopedic Surgeon", city: "Mumbai" },
+    { id: 2, image: "https://media.istockphoto.com/id/177373093/photo/indian-male-doctor.jpg?s=612x612&w=0&k=20&c=5FkfKdCYERkAg65cQtdqeO_D0JMv6vrEdPw3mX1Lkfg=", name: "Dr. Gawande", expertise: "Cardiologist", city: "Mumbai" },
+    { id: 3, image: "https://t4.ftcdn.net/jpg/03/20/52/31/360_F_320523164_tx7Rdd7I2XDTvvKfz2oRuRpKOPE5z0ni.jpg", name: "Dr. Patel", expertise: "Dermatologist", city: "Pune" },
+    { id: 4, image: "https://media.istockphoto.com/id/1446041217/photo/portrait-of-smiling-male-doctor-on-white-background.webp?b=1&s=170667a&w=0&k=20&c=t1y5xibAPFIzOa1oN4tQ5RQKSiURBU2VxeJahOSw9wE=", name: "Dr. Sam", expertise: "Orthologist", city: "Pune" },
+    { id: 5, image: "https://st.depositphotos.com/2702761/3304/i/450/depositphotos_33044395-stock-photo-doctor-smiling.jpg", name: "Dr. AB Patel", expertise: "Cardiologist", city: "Pune" },
+    { id: 6, image: "https://img.freepik.com/free-photo/handsome-young-male-doctor-with-stethoscope-standing-against-blue-background_662251-343.jpg", name: "Dr.Saurabh", expertise: "Neurologist", city: "Pune" },
+    { id: 7, image: "https://img.freepik.com/free-photo/handsome-young-male-doctor-with-stethoscope-standing-against-blue-background_662251-343.jpg", name: "Dr. John Doe", expertise: "Nephrologist", city: "Pune" },
   ];
 
 app.get('/api/doctors', (req, res) => {
